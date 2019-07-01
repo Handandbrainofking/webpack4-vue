70a80259ec380dc6b205f693f9c0726e121bf686
yourname <602204582@qq.com>
2019-07-01 11:24:00 +0800

    less使用方法

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="app">
-        <span>{{msg}}</span>
+        <h3 class="logo-text-link"><a href="javascript: void 0">{{msg}}</a></h3>
         <img class="logo" src="./assets/img/logo.png"/>
     </div>
 </template>
@@ -14,13 +14,32 @@ export default {
 }
 </script>
 <style lang='less' scoped>
-.logo {
-    width: 100px;
-    height: 100px;
-    border-radius: 8px;
-    border: 1px solid #efefef;
-    margin: 10px 20px;
+@width: 200px;
+@height: 200px + 20px;
+@bgColor: #99c24e;
+@logo-selector: logo;
+@text-logo: logo-text-link;
+.app {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    .@{text-logo} {
+        font-size: 20px;
+        a {
+            color: #0092d7;
+        }
+    }
+    .@{logo-selector} {
+        width: @width;
+        height: @height;
+        border-radius: 8px;
+        border: 1px solid #efefef;
+        margin: 10px 20px;
+        background: @bgColor;
+    }
 }
+
 </style>
 
 
