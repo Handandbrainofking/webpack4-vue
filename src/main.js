0468e1e5197e6612c08cbfc9a962914a266f3509
yourname <602204582@qq.com>
2019-07-02 16:26:49 +0800

    全局变量

--- src/main.js
+++ src/main.js
@@ -4,6 +4,11 @@ console.log('Running App version ' + VERSION);
 console.log('Is production Environment ' + PRODUCTION); 
 console.log(process.env.NODE_ENV)
 
+if(process.env.NODE_ENV === 'production') {
+    console.log('envrionment: prodction')
+} else {
+    console.log('envrionment: development')
+}
 var app = new Vue({
     el: '#app',
     components: { App },
